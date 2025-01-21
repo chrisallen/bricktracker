a3e50e9b3c4d5ee2f2ba6c9d9e4e586ba2bb1a07
Gregoo <versatile.mailbox@gmail.com>
2025-01-21 17:25:49 +0100

    Fix indent

--- bricktracker/sql/migrations/0001.sql
+++ bricktracker/sql/migrations/0001.sql
@@ -3,60 +3,64 @@
 BEGIN transaction;
 
 CREATE TABLE IF NOT EXISTS "wishlist" (
-        "set_num" TEXT,
-        "name" TEXT,
-        "year" INTEGER,
-        "theme_id" INTEGER,
-        "num_parts" INTEGER,
-        "set_img_url" TEXT,
-        "set_url" TEXT,
-        "last_modified_dt" TEXT
-    );
+    "set_num" TEXT,
+    "name" TEXT,
+    "year" INTEGER,
+    "theme_id" INTEGER,
+    "num_parts" INTEGER,
+    "set_img_url" TEXT,
+    "set_url" TEXT,
+    "last_modified_dt" TEXT
+);
+
 CREATE TABLE IF NOT EXISTS "sets" (
-        "set_num" TEXT,
-        "name" TEXT,
-        "year" INTEGER,
-        "theme_id" INTEGER,
-        "num_parts" INTEGER,
-        "set_img_url" TEXT,
-        "set_url" TEXT,
-        "last_modified_dt" TEXT,
-        "mini_col" BOOLEAN,
-        "set_check" BOOLEAN,
-        "set_col" BOOLEAN,
-        "u_id" TEXT
-    );
+    "set_num" TEXT,
+    "name" TEXT,
+    "year" INTEGER,
+    "theme_id" INTEGER,
+    "num_parts" INTEGER,
+    "set_img_url" TEXT,
+    "set_url" TEXT,
+    "last_modified_dt" TEXT,
+    "mini_col" BOOLEAN,
+    "set_check" BOOLEAN,
+    "set_col" BOOLEAN,
+    "u_id" TEXT
+);
+
 CREATE TABLE IF NOT EXISTS "inventory" (
-        "set_num" TEXT,
-        "id" INTEGER,
-        "part_num" TEXT,
-        "name" TEXT,
-        "part_img_url" TEXT,
-        "part_img_url_id" TEXT,
-        "color_id" INTEGER,
-        "color_name" TEXT,
-        "quantity" INTEGER,
-        "is_spare" BOOLEAN,
-        "element_id" INTEGER,
-        "u_id" TEXT
-    );
+    "set_num" TEXT,
+    "id" INTEGER,
+    "part_num" TEXT,
+    "name" TEXT,
+    "part_img_url" TEXT,
+    "part_img_url_id" TEXT,
+    "color_id" INTEGER,
+    "color_name" TEXT,
+    "quantity" INTEGER,
+    "is_spare" BOOLEAN,
+    "element_id" INTEGER,
+    "u_id" TEXT
+);
+
 CREATE TABLE IF NOT EXISTS "minifigures" (
-        "fig_num" TEXT,
-        "set_num" TEXT,
-        "name" TEXT,
-        "quantity" INTEGER,
-        "set_img_url" TEXT,
-        "u_id" TEXT
-    );
+    "fig_num" TEXT,
+    "set_num" TEXT,
+    "name" TEXT,
+    "quantity" INTEGER,
+    "set_img_url" TEXT,
+    "u_id" TEXT
+);
+
 CREATE TABLE IF NOT EXISTS "missing" (
-        "set_num" TEXT,
-        "id" INTEGER,
-        "part_num" TEXT,
-        "part_img_url_id" TEXT,
-        "color_id" INTEGER,
-        "quantity" INTEGER,
-        "element_id" INTEGER,
-        "u_id" TEXT
-    );
+    "set_num" TEXT,
+    "id" INTEGER,
+    "part_num" TEXT,
+    "part_img_url_id" TEXT,
+    "color_id" INTEGER,
+    "quantity" INTEGER,
+    "element_id" INTEGER,
+    "u_id" TEXT
+);
 
 COMMIT;
