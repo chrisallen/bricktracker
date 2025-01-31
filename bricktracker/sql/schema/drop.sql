418a332f0396dd6c689057c0abdefc4c0d1b0a92
Gregoo <versatile.mailbox@gmail.com>
2025-01-31 18:09:06 +0100

    Add missing set owners SQL drop

--- bricktracker/sql/schema/drop.sql
+++ bricktracker/sql/schema/drop.sql
@@ -1,11 +1,13 @@
 BEGIN transaction;
 
+DROP TABLE IF EXISTS "bricktracker_metadata_owners";
 DROP TABLE IF EXISTS "bricktracker_metadata_statuses";
 DROP TABLE IF EXISTS "bricktracker_metadata_tags";
 DROP TABLE IF EXISTS "bricktracker_minifigures";
 DROP TABLE IF EXISTS "bricktracker_parts";
 DROP TABLE IF EXISTS "bricktracker_sets";
 DROP TABLE IF EXISTS "bricktracker_set_checkboxes";
+DROP TABLE IF EXISTS "bricktracker_set_owners";
 DROP TABLE IF EXISTS "bricktracker_set_statuses";
 DROP TABLE IF EXISTS "bricktracker_set_storages";
 DROP TABLE IF EXISTS "bricktracker_set_tags";
