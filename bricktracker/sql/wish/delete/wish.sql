69318e7b0b157a969a87e5a498a40a4fb7e08654
FrederikBaerentsen <frederik+gitea@baerentsen.net>
2026-04-17 14:42:40 +0200

    fix(wishes): delete wish owners before wish to avoid FK constraint error

--- bricktracker/sql/wish/delete/wish.sql
+++ bricktracker/sql/wish/delete/wish.sql
@@ -3,10 +3,10 @@
 
 BEGIN TRANSACTION;
 
-DELETE FROM "bricktracker_wishes"
-WHERE "bricktracker_wishes"."set" IS NOT DISTINCT FROM '{{ set }}';
-
 DELETE FROM "bricktracker_wish_owners"
 WHERE "bricktracker_wish_owners"."set" IS NOT DISTINCT FROM '{{ set }}';
+
+DELETE FROM "bricktracker_wishes"
+WHERE "bricktracker_wishes"."set" IS NOT DISTINCT FROM '{{ set }}';
 
 COMMIT;
